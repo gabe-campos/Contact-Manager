eae38d491b69851469e3c3c49406f9ccbf5daaf9
gabe-campos <91922397+gabe-campos@users.noreply.github.com>
2023-06-20 21:36:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: 'your_host',
-    user: 'your_username',
-    password: 'your_password',
-    database: 'your_database_name',
+    host: 'your_host',                    // REPLACE
+    user: 'your_username',                // REPLACE
+    password: 'your_password',            // REPLACE
+    database: 'your_database_name',       // REPLACE
   };
   
