7ef32d1ef55cc1fe6d12a6c6384ba2cbcc6d73b0
gabe-campos <91922397+gabe-campos@users.noreply.github.com>
2023-06-23 19:02:25 -0500

    Update insert_into_accounts_table.js

--- insert_into_accounts_table.js
+++ insert_into_accounts_table.js
@@ -3,10 +3,10 @@ const bcrypt = require('bcrypt');
 const config = require('./config.js');
 
 const dbCon = mysql.createConnection({
-  host: config.host,               // REPLACE 
-  user: config.user,               // REPLACE
-  password: config.password,       // REPLACE
-  database: config.database,       // REPLACE
+  host: config.host,               
+  user: config.user,               
+  password: config.password,       
+  database: config.database,       
   port: 3306                  // tpyical port for js
 });
 
